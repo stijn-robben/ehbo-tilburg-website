0119ed665dcc0baa8d7e3159b2e4685e62b097a7
AmarDuran <amarduran2002@gmail.com>
2023-06-02 10:55:44 +0200

    regex gefixt

--- register.php
+++ register.php
@@ -14,11 +14,28 @@ if (isset($_POST['submit'])) {
 }
 ?>
 
-<!-- Controleer of het wachtwoord aan de vereisten voldoet met behulp van de regex
+<?php
+if (isset($_POST['submit'])) {
+    $wachtwoord = $_POST['wachtwoord'];
+    $voornaam = $_POST['voornaam'];
+    $achternaam = $_POST['achternaam'];
+    $email = $_POST['email'];
+
+    // Controleer of het wachtwoord aan de vereisten voldoet met behulp van de regex
     $password_regex = '/^(?=.*\d)(?=.*[!@#$%^&*])\S{8,}/';
     if (!preg_match($password_regex, $wachtwoord)) {
-        echo "<h1>Inloggen mislukt</h1>";
-        echo "<p>
-        Ongeldig wachtwoord. Zorg ervoor dat je wachtwoord minimaal 8 tekens bevat, inclusief minimaal 1 cijfer en 1 speciaal teken.</p>";
+        echo "<h1>Registratie mislukt</h1>";
+        echo "<p>Ongeldig wachtwoord. Zorg ervoor dat je wachtwoord minimaal 8 tekens bevat, inclusief minimaal 1 cijfer en 1 speciaal teken.</p>";
         return;
-    } -->
+    }
+
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $dbname = 'ehbotest'; // Naam van de lokale testdatabase
+    $conn = mysqli_connect($host, $user, $pass, $dbname);
+    $sql = "INSERT INTO gebruiker(wachtwoord,voornaam,achternaam,email) values ('$wachtwoord', '$voornaam', '$achternaam', '$email')";
+    mysqli_query($conn, $sql);
+}
+?>
+

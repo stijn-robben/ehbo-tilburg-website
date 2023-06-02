0f05f42ea9a123bd9458fbd1bb05b14a7ca19ff5
DavitDashyan <davit.dashyan@icloud.com>
2023-06-02 16:30:25 +0200

    Login werkt op website via online mysql

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+
+    try{
+        $dsn = "mysql: dbname=id19625723_ehbo; host=localhost";
+        $user = "id19625723_root";
+        $pswd = "ehboAvans23!";
+
+        $conn = new PDO($dsn, $user, $pswd);
+
+        $conn->query("USE id19625723_ehbo");
+    }
+    catch(PDOExeption $e){
+        die("Error connecting: ".$e->getMessage());
+    }
+?>

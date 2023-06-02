799b91dc07269e2be64f3b3abc31bdf390f578d1
AmarDuran <amarduran2002@gmail.com>
2023-06-02 11:32:44 +0200

    Hashed password
    Hier is de code aangepast zodat het wachtwoord tijdens het regristreren gehashed wordt.

--- register.php
+++ register.php
@@ -1,15 +1,18 @@
 <?php
 if (isset($_POST['submit'])) {
     $wachtwoord = $_POST['wachtwoord'];
-    $voornaam = $_POST['voornaam'];
-    $achternaam = $_POST['achternaam'];
+    $firstname = $_POST['voornaam'];
+    $lastname = $_POST['achternaam'];
     $email = $_POST['email'];
     $host = 'localhost';
     $user = 'root';
     $pass = '';
     $dbname = 'ehbotest'; // Naam van de local test database
     $conn = mysqli_connect($host, $user, $pass, $dbname);
-    $sql = "INSERT INTO gebruiker(wachtwoord,voornaam,achternaam,email) values ('$wachtwoord', '$voornaam', '$achternaam', '$email')";
+    $hashedPassword = password_hash($wachtwoord, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO gebruiker (wachtwoord, voornaam, achternaam, email) VALUES ('$hashedPassword', '$firstname', '$lastname', '$email')";
+
+    //$sql = "INSERT INTO gebruiker(wachtwoord,voornaam,achternaam,email) values ('$wachtwoord', '$voornaam', '$achternaam', '$email')";
     mysqli_query($conn, $sql);
 }
 ?>
@@ -38,4 +41,3 @@ if (isset($_POST['submit'])) {
     mysqli_query($conn, $sql);
 }
 ?>
-

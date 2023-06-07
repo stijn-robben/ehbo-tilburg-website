4e1720b16f4799b99ab57bbb69e8e553fa4550b0
AmarDuran <amarduran2002@gmail.com>
2023-06-07 09:17:23 +0200

    Cursus toevoegen aangepast
    Zelfde manier als davit geprobeerd

--- cursusToevoegen.php
+++ cursusToevoegen.php
@@ -5,12 +5,31 @@ if (isset($_POST['submit'])) {
     $enrollments = $_POST['enrollments'];
     $keywords = $_POST['keywords'];
 
+    $conn = mysqli_connect($host, $user, $pass, $dbname);
+
     $host = 'db-mysql-ams3-46626-do-user-8155278-0.b.db.ondigitalocean.com';
     $user = 'Knv-ehbo-tilburg';
     $pass = '3HBO!';
     $dbname = 'Knv-ehbo-tilburg';
-    $conn = mysqli_connect($host, $user, $pass, $dbname);
+    $port = 25060;
+
+    $conn = mysqli_init();
+    mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
+    mysqli_real_connect($conn, $host, $user, $pass, $dbname, $port);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
     $sql = "INSERT INTO cursus (subject, date, enrollments, keywords) VALUES ('$subject', '$date', '$enrollments', '$keywords')";
-    mysqli_query($conn, $sql);
+   
+    if (mysqli_query($conn, $sql)) {
+        echo "Cursus toegevoegd.";
+    } else {
+        echo "Fout bij het toevoegen van een cursus." . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+
 }
 ?>

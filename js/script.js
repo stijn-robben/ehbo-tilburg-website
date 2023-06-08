cf749f07e93e03e12ce7bd1561668f074333b9b6
Stefan <stefandekkers2005@gmail.com>
2023-06-08 10:56:24 +0200

    navbar + go back

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 function goBack() {
     window.history.back();
 }
+
+window.addEventListener('DOMContentLoaded', function () {
+    var navbarPlaceholder = document.getElementById('navbar-placeholder');
+
+    // Create a new XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            // Inject the navbar HTML into the placeholder div
+            navbarPlaceholder.innerHTML = xhr.responseText;
+        }
+    };
+
+    // Load the navbar.html file
+    xhr.open('GET', 'navbar.html', true);
+    xhr.send();
+});

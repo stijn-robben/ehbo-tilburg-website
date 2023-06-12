d1ff3152aed05ec339168a62fd383ced972e95ad
DavitDashyan <davit.dashyan@icloud.com>
2023-06-12 14:28:50 +0200

    Approve werkt

--- approved.php
+++ approved.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,17 +8,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Website van KNV EHBO Tilburg." />
     <meta name="robots" content="index, follow" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
     <link rel="stylesheet" href="./css/style.css" />
     <link rel="icon" type="image/x-icon" href="/img/s.png" />
     <script defer src="./js/script.js"></script>
-    <title>EHBO Tilburg - Cursussen</title>
+    <title>EHBO Tilburg - Approved</title>
 </head>
 
 <body class="bg-light">
     <?php
-    session_start();
-
+    // session_start();
+    
     // Connect to the database
     $host = 'db-mysql-ams3-46626-do-user-8155278-0.b.db.ondigitalocean.com';
     $port = 25060;
@@ -35,19 +37,24 @@
     }
 
     // Fetch courses from the database
-    $query = "SELECT * FROM course WHERE date >= CURDATE() ORDER BY date ASC";
+    $query = "SELECT * FROM user WHERE approved = 0";
     $result = mysqli_query($conn, $query);
 
     // Generate HTML for courses
     $coursesHTML = "";
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            $date = date('d-m-Y', strtotime($row["date"]));
-            $subject = $row["subject"];
-            $keywords = $row["keywords"];
-            $max_enrollments = $row["max_enrollments"];
-            $enrollments = $row["enrollments"];
-            $enrollments_text = $enrollments . "/" . $max_enrollments;
+            // $date = date('d-m-Y', strtotime($row["date"]));
+            // $subject = $row["subject"];
+            // $keywords = $row["keywords"];
+            // $max_enrollments = $row["max_enrollments"];
+            // $enrollments = $row["enrollments"];
+            // $enrollments_text = $enrollments . "/" . $max_enrollments;
+    
+            $id = $row['id_user'];
+            $voornaam = $row['firstname'];
+            $achternaam = $row['lastname'];
+            $approved = $row['approved'];
 
             $courseHTML = '
             <div class="row">
@@ -56,19 +63,26 @@
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-2">
-                                    <p class="card-text">' . $date . '</p>
+                                    <p class="card-text">' . $id . '</p>
                                 </div>
                                 <div class="col-md-2">
-                                    <p class="card-text">' . $subject . '</p>
+                                    <p class="card-text">' . $voornaam . '</p>
                                 </div>
                                 <div class="col-md-3">
-                                    <p class="card-text">' . $keywords . '</p>
+                                    <p class="card-text">' . $achternaam . '</p>
                                 </div>
                                 <div class="col-md-2">
-                                    <p class="card-text">' . $enrollments_text . '</p>
+                                    <p class="card-text">' . $approved . '</p>
                                 </div>
                                 <div class="col-md-3 text-center">
-                                    <button class="btn btn-sm btn-primary">Inschrijven</button>
+                                    <form action="" method="post" class="d-inline-block">
+                                        <input type="hidden" name="id" value="' . $id . '">
+                                        <button type="submit" name="approve" class="btn btn-sm btn-primary">ㅤㅤ✓ㅤㅤ</button>
+                                    </form>
+                                    <form action="" method="post" class="d-inline-block">
+                                        <input type="hidden" name="id" value="' . $id . '">
+                                        <button type="submit" name="delete" class="btn btn-sm btn-primary">ㅤㅤXㅤㅤ</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
@@ -79,10 +93,30 @@
             $coursesHTML .= $courseHTML;
         }
     }
+    // Approve button action
+    if (isset($_POST['approve'])) {
+        $id = $_POST['id'];
+        $updateQuery = "UPDATE user SET approved = 1 WHERE id_user = $id";
+        mysqli_query($conn, $updateQuery);
+        // Redirect to the same page to refresh the content
+        // header("Location: " . $_SERVER['PHP_SELF']);
+        exit();
+    }
+
+    // Delete button action
+    if (isset($_POST['delete'])) {
+        $id = $_POST['id'];
+        $deleteQuery = "DELETE FROM user WHERE id_user = $id";
+        mysqli_query($conn, $deleteQuery);
+        // Redirect to the same page to refresh the content
+        // header("Location: " . $_SERVER['PHP_SELF']);
+        exit();
+    }
 
     // Close the database connection
     $conn->close();
     ?>
+
 
     <!--Navbar-->
     <div id="navbar-placeholder"></div>
@@ -92,11 +126,11 @@
         <a class="btn btn-secondary btn-lg go-back" onclick="goBack()" role="button"> Ga terug </a>
     </div>
 
-    <!-- Cursussen -->
+    <!-- approved -->
     <div class="jumbotron bg-jumbotron">
         <div class="container">
             <div class="container">
-                <p class="jumbotron-head h2-secondary">Cursussen</p>
+                <p class="jumbotron-head h2-secondary">Approved</p>
             </div>
             <div class="container">
                 <div class="row">
@@ -105,16 +139,16 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-md-2">
-                                        <h5 class="text-main">Datum</h5>
+                                        <h5 class="text-main">Id</h5>
                                     </div>
                                     <div class="col-md-2">
-                                        <h5 class="text-main">Onderwerp</h5>
+                                        <h5 class="text-main">Voornaam</h5>
                                     </div>
                                     <div class="col-md-3">
-                                        <h5 class="text-main">Competenties</h5>
+                                        <h5 class="text-main">Achternaam</h5>
                                     </div>
                                     <div class="col-md-2">
-                                        <h5 class="text-main">Aantal inschrijvingen</h5>
+                                        <h5 class="text-main">Approved</h5>
                                     </div>
                                 </div>
                             </div>
@@ -129,7 +163,9 @@
     <!--Footer-->
     <div id="footer-placeholder"></div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>

415b14bc95d80ba22026d7708bdf4773449921e7
Stijn Robben <stijn.robben2005@gmail.com>
2023-06-12 13:45:22 +0200

    Added captcha on Account aanvragen and Contact

--- account-aanvragen.php
+++ account-aanvragen.php
@@ -11,46 +11,70 @@ if (isset($_POST['submit'])) {
     $description = $_POST['description'];
     $password = $_POST['password'];
 
-    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+    $recaptcha_secret = '6Lfe7Y0mAAAAAALHCbs8FHqgfsHoFUKuT0Mc8EEF
+    ';
+    $response = $_POST['g-recaptcha-response'];
 
-    // Controleer of het wachtwoord aan de vereisten voldoet met behulp van de regex
-    $password_regex = '/^(?=.*\d)(?=.*[!@#$%^&*])\S{8,}/';
-    if (!preg_match($password_regex, $password)) {
-        echo "<h1>Registratie mislukt</h1>";
-        echo "<p>Ongeldig wachtwoord. Zorg ervoor dat je wachtwoord minimaal 8 tekens bevat, minimaal 1 cijfer en 1 speciaal teken.</p>";
-        return;
-    }
+    // Make a POST request to verify the reCAPTCHA response
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+        'secret' => $recaptcha_secret,
+        'response' => $response
+    ]));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $recaptcha_result = curl_exec($ch);
+    curl_close($ch);
 
-    $postcode_regex = '/^[1-9][0-9]{3}\s?[a-zA-Z]{2}$/';
-    if (preg_match($postcode_regex, $postal)) {
-        echo "De postcode voldoet aan de eisen.";
+    $recaptcha_result = json_decode($recaptcha_result);
+
+    if (!$recaptcha_result->success) {
+        echo '<script>alert("reCAPTCHA verification failed. Please try again.");</script>';
     } else {
-        echo "De postcode voldoet niet aan de eisen.";
-    }
 
-    $host = 'db-mysql-ams3-46626-do-user-8155278-0.b.db.ondigitalocean.com';
-    $user = 'Knv-ehbo-tilburg';
-    $pass = '3HBO!';
-    $dbname = 'Knv-ehbo-tilburg';
-    $port = 25060;
 
-    $conn = mysqli_init();
-    mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
-    mysqli_real_connect($conn, $host, $user, $pass, $dbname, $port);
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO user (firstname, lastname, email, postal, city, address, description, password) 
+        // Controleer of het wachtwoord aan de vereisten voldoet met behulp van de regex
+        $password_regex = '/^(?=.*\d)(?=.*[!@#$%^&*])\S{8,}/';
+        if (!preg_match($password_regex, $password)) {
+            echo "<h1>Registratie mislukt</h1>";
+            echo "<p>Ongeldig wachtwoord. Zorg ervoor dat je wachtwoord minimaal 8 tekens bevat, minimaal 1 cijfer en 1 speciaal teken.</p>";
+            return;
+        }
+
+        $postcode_regex = '/^[1-9][0-9]{3}\s?[a-zA-Z]{2}$/';
+        if (preg_match($postcode_regex, $postal)) {
+            echo "De postcode voldoet aan de eisen.";
+        } else {
+            echo "De postcode voldoet niet aan de eisen.";
+        }
+
+        $host = 'db-mysql-ams3-46626-do-user-8155278-0.b.db.ondigitalocean.com';
+        $user = 'Knv-ehbo-tilburg';
+        $pass = '3HBO!';
+        $dbname = 'Knv-ehbo-tilburg';
+        $port = 25060;
+
+        $conn = mysqli_init();
+        mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
+        mysqli_real_connect($conn, $host, $user, $pass, $dbname, $port);
+
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "INSERT INTO user (firstname, lastname, email, postal, city, address, description, password) 
             VALUES ('$firstname', '$lastname', '$email', '$postal', '$city', '$address', '$description', '$password_hash')";
 
-    if (mysqli_query($conn, $sql)) {
-        echo "Account is aangemaakt.";
-    } else {
-        echo "Fout bij het aanmaken van het account: " . mysqli_error($conn);
-    }
+        if ($conn->query($sql) === TRUE) {
+            echo '<script>alert("Uw nieuwe account is aangevraagd!");</script>';
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
 
-    mysqli_close($conn);
+        mysqli_close($conn);
+    }
 }
-?>
--- contact.php
+++ contact.php
@@ -11,30 +11,52 @@ $dbname = 'Knv-ehbo-tilburg';
 // Verbinding maken met de database
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 
-// Controleren op fouten bij de verbinding
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$recaptcha_secret = '6Lfe7Y0mAAAAAALHCbs8FHqgfsHoFUKuT0Mc8EEF
+';
+$response = $_POST['g-recaptcha-response'];
 
-// Controleren of het formulier is verzonden
-if (isset($_POST["submit"])) {
-    // Gegevens uit het formulier halen
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $telefoon = $_POST["telefoon"];
-    $description = $_POST["description"];
+// Make a POST request to verify the reCAPTCHA response
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+    'secret' => $recaptcha_secret,
+    'response' => $response
+]));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$recaptcha_result = curl_exec($ch);
+curl_close($ch);
 
-    // Query om de gegevens in de database in te voegen
-    $sql = "INSERT INTO contact (name, email, phonenumber, message) VALUES ('$name', '$email', '$telefoon', '$description')";
+$recaptcha_result = json_decode($recaptcha_result);
 
-    // Controleren of de query succesvol is uitgevoerd
-    if ($conn->query($sql) === TRUE) {
-        echo "Bedankt voor je bericht!";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+if (!$recaptcha_result->success) {
+    // The reCAPTCHA verification failed
+    echo '<script>alert("reCAPTCHA verification failed. Please try again.");</script>';
+} else {
+    // Controleren op fouten bij de verbinding
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-}
 
-// Verbinding met de database sluiten
-$conn->close();
-?>
+    // Controleren of het formulier is verzonden
+    if (isset($_POST["submit"])) {
+        // Gegevens uit het formulier halen
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $telefoon = $_POST["telefoon"];
+        $description = $_POST["description"];
+
+        // Query om de gegevens in de database in te voegen
+        $sql = "INSERT INTO contact (name, email, phonenumber, message) VALUES ('$name', '$email', '$telefoon', '$description')";
+
+        // Controleren of de query succesvol is uitgevoerd
+        if ($conn->query($sql) === TRUE) {
+            echo '<script>alert("Bedankt voor je bericht!");</script>';
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+
+    // Verbinding met de database sluiten
+    $conn->close();
+}

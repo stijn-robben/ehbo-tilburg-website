09d82187f3479585090f10c26e9862bcce5b5970
Stefan <stefandekkers2005@gmail.com>
2023-06-20 13:21:24 +0200

    cursus toevoegen

--- beheer.php
+++ beheer.php
@@ -49,7 +49,7 @@ if (isset($_SESSION['role'])) {
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title text-secondary" id="cursusToevoegenModalLabel">Cursus beheren</h5>
+                                    <h5 class="modal-title text-secondary" id="cursusToevoegenModalLabel">Cursus toevoegen</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
